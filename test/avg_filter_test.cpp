a3a5b4ed6adcfa494ef84ae50439884ecca789ba
graiola <gennaro.raiola@gmail.com>
2022-03-10 11:52:23 +0100

    Add variance test for the moving avg filter

--- test/avg_filter_test.cpp
+++ test/avg_filter_test.cpp
@@ -3,25 +3,26 @@
 #include "target_estimation/utils.hpp"
 
 // Simulated noise
-const double stddev = 1;
-const double mean = 5.0;
+const double gt_stddev = 1;
+const double gt_var    = gt_stddev*gt_stddev;
+const double gt_mean = 5.0;
 // Statistical generators
 static std::default_random_engine generator;
-static std::normal_distribution<double> normal_dist(mean,stddev);
+static std::normal_distribution<double> normal_dist(gt_mean,gt_stddev);
 
 TEST(test_filter, AvgFilter )
 {
   AvgFilter filter(1000);
 
   unsigned int n_meas = 10000;
-  Eigen::VectorXd res(n_meas);
+  Eigen::VectorXd mean(n_meas);
   double meas;
   for(unsigned int i=0;i<n_meas;i++)
   {
       meas = normal_dist(generator);
-      res(i) = filter.update(meas);
+      mean(i) = filter.update(meas);
   }
-  EXPECT_NEAR( mean ,res(n_meas-1), 0.1 );
+  EXPECT_NEAR( gt_mean ,mean(n_meas-1), 0.1 );
 }
 
 TEST(test_filter, MovingAvgFilter )
@@ -29,14 +30,17 @@ TEST(test_filter, MovingAvgFilter )
   MovingAvgFilter filter(1000);
 
   unsigned int n_meas = 10000;
-  Eigen::VectorXd res(n_meas);
+  Eigen::VectorXd mean(n_meas);
+  Eigen::VectorXd var(n_meas);
   double meas;
   for(unsigned int i=0;i<n_meas;i++)
   {
       meas = normal_dist(generator);
-      res(i) = filter.update(meas);
+      mean(i) = filter.update(meas);
+      var(i) = filter.getVariance();
   }
-  EXPECT_NEAR( mean ,res(n_meas-1), 0.1 );
+  EXPECT_NEAR( gt_mean ,mean(n_meas-1), 0.1 );
+  EXPECT_NEAR( gt_var , var(n_meas-1), 0.1 );
 }
 
 int main(int argc, char** argv)
